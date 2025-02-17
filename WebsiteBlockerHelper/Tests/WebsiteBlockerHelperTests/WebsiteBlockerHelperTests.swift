13da043c6587f70adcab9763461c35076fe21b4b
shashwatsingh <shashwat.singh0406@gmail.com>
2025-02-18 05:23:41 +0530

    Ignored build files to push CLI helper to remote repo

---
+++ WebsiteBlockerHelper/Tests/WebsiteBlockerHelperTests/WebsiteBlockerHelperTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import WebsiteBlockerHelper
+
+final class WebsiteBlockerHelperTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(WebsiteBlockerHelper().text, "Hello, World!")
+    }
+}
